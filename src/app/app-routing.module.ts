aabb57096cf93a62de6c260714c742bda36d4204
haridy_revival <a.haridy.revival@gmail.com>
2019-09-05 18:05:23 +0200

    init ng contact app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,44 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { ExtraOptions, Routes, RouterModule } from '@angular/router';
 
+import { ContactsListComponent } from './components/contacts-list/contacts-list.component';
+import { ContactsAddComponent } from './components/contacts-add/contacts-add.component';
+import { ContactsEditComponent } from './components/contacts-edit/contacts-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  // App routes goes here 
+  {
+    path: '',
+    component: ContactsListComponent,
+    data: { title: 'Contacts List' }
+  },
+  {
+    path: 'contacts',
+    component: ContactsListComponent,
+    data: { title: 'Contacts List' }
+  },
+  {
+    path: 'contacts/add',
+    component: ContactsAddComponent,
+    data: { title: 'Add new Contact' }
+  },
+  {
+    path: 'contacts/edit/:id',
+    component: ContactsEditComponent,
+    data: { title: 'Edit contact' }
+  },
+  { path: '**', component: ContactsListComponent },
+
+];
+
+const config: ExtraOptions = {
+  useHash: true,
+};
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, config)],
+  exports: [RouterModule],
 })
+
 export class AppRoutingModule { }
