ac9aa916f0a86c38bdc9285294c5346573ac02fa
Ahmad ElSokkary <ahmed.sokkary@gmail.com>
2019-09-08 12:08:47 +1000

    add,update and delete features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ModalModule } from "ngx-bootstrap";
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
+//services
+import { ContactsService } from './services/contacts.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactsListComponent } from './components/contacts-list/contacts-list.component';
+import { ContactsAddComponent } from './components/contacts-add/contacts-add.component';
+import { ContactsEditComponent } from './components/contacts-edit/contacts-edit.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsListComponent,
+    ContactsAddComponent,
+    ContactsEditComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ModalModule.forRoot(),
+    Ng2SearchPipeModule
   ],
-  providers: [],
+  providers: [ContactsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
